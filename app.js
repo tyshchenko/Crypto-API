03f911862adc4854e18bdfd5bb968e3a2fa7e414
pinokara <hec.stw@gmail.com>
2019-05-27 01:23:26 +0700

    Main app.js

--- app.js
+++ app.js
@@ -1,63 +1,68 @@
 var express = require('express')
   , path = require('path')
   , chaincoinapi = require('chaincoin-node-api')
-  , favicon = require('static-favicon')
   , logger = require('morgan')
   , cookieParser = require('cookie-parser')
   , bodyParser = require('body-parser')
   , settings = require('./lib/settings')
-  , routes = require('./routes/index')
   , lib = require('./lib/explorer')
   , db = require('./lib/database')
-  , locale = require('./lib/locale')
-  , request = require('request');
-
+var routes = require('./routes/index')
+var debug = require('debug')('explorer');
 var app = express();
+app.set('port', process.env.PORT || settings.port);
 
-// chaincoinapi
-chaincoinapi.setWalletDetails(settings.wallet);
-if (settings.heavy != true) {
-  chaincoinapi.setAccess('only', ['getinfo', 'getnetworkhashps', 'getmininginfo','getdifficulty', 'getconnectioncount',
-  'getmasternodecount', 'getmasternodecountonline', 'getmasternodelist', 'getvotelist', 'getblockcount', 'getblockhash', 'getblock', 'getrawtransaction', 
-  'getpeerinfo', 'gettxoutsetinfo']);
-} else {
-  // enable additional heavy api calls
-  /*
-    getvote - Returns the current block reward vote setting.
-    getmaxvote - Returns the maximum allowed vote for the current phase of voting.
-    getphase - Returns the current voting phase ('Mint', 'Limit' or 'Sustain').
-    getreward - Returns the current block reward, which has been decided democratically in the previous round of block reward voting.
-    getnextrewardestimate - Returns an estimate for the next block reward based on the current state of decentralized voting.
-    getnextrewardwhenstr - Returns string describing how long until the votes are tallied and the next block reward is computed.
-    getnextrewardwhensec - Same as above, but returns integer seconds.
-    getsupply - Returns the current money supply.
-    getmaxmoney - Returns the maximum possible money supply.
-  */
-  chaincoinapi.setAccess('only', ['getinfo', 'getstakinginfo', 'getnetworkhashps', 'getdifficulty', 'getconnectioncount',
-    'getmasternodecount', 'getmasternodecountonline', 'getmasternodelist', 'getvotelist', 'getblockcount', 'getblockhash', 
-    'getblock', 'getrawtransaction', 'getmaxmoney', 'getvote', 'getmaxvote', 'getphase', 'getreward', 'getpeerinfo', 
-    'getnextrewardestimate', 'getnextrewardwhenstr', 'getnextrewardwhensec', 'getsupply', 'gettxoutsetinfo']);
-}
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 
-app.use(favicon(path.join(__dirname, settings.favicon)));
+settings.coin.map(i=>{
+    chaincoinapi.setWalletDetails(i.wallet);
+    chaincoinapi.setAccess('only', ['getinfo', 'getstakinginfo', 'getnetworkhashps', 'getdifficulty', 'getconnectioncount',
+        'getmasternodecount', 'getmasternodecountonline', 'getmasternodelist', 'getvotelist', 'getblockcount', 'getblockhash',
+        'getblock', 'getrawtransaction', 'getmaxmoney', 'getvote', 'getmaxvote', 'getphase', 'getreward', 'getpeerinfo',
+        'getnextrewardestimate', 'getnextrewardwhenstr', 'getnextrewardwhensec', 'getsupply', 'gettxoutsetinfo']);
+    app.use(`/api/${i.name}`, chaincoinapi.app);
+})
+
+// app.use('/', routes)
+
+// app.use(favicon(path.join(__dirname, settings.favicon)));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+// app.get('/', function (req, res) {
+//     res.send('hello world')
+// })
+
+Promise.all(settings.coin.map(i => {
+    return db.connect(i.name)
+})).then(data => {
+    // app.get('/', function (req, res) {
+    //     res.send('hello world')
+    // })
+    data.map(i => {
+        require('./bin/instance')(i)
+    })
+}).then(()=>{
+    app.listen(app.get('port'), function() {
+        debug('Express server listening on port ' + settings.port);
+    });
+})
+Promise.all(settings.coin.map(i=>{
+    db.connect(i.name).then(conn=>{
+        console.log(conn.coin)
+        routes(conn).then(router => app.use(`/coin/${i.name}`, router));
+    })
+}))
 
 // routes
-app.use('/api', chaincoinapi.app);
-app.use('/', routes);
+
 app.use('/ext/getmoneysupply', function(req,res){
   lib.get_supply(function(supply){
     res.send(' '+supply);
   });
 });
-
+//
 app.use('/ext/getaddress/:hash', function(req,res){
   db.get_address(req.param('hash'), function(address){
     if (address) {
@@ -107,58 +112,32 @@ app.use('/ext/connections', function(req,res){
   });
 });
 
-// locals
-app.set('title', settings.title);
-app.set('symbol', settings.symbol);
-app.set('coin', settings.coin);
-app.set('locale', locale);
-app.set('display', settings.display);
-app.set('markets', settings.markets);
-app.set('twitter', settings.twitter);
-app.set('facebook', settings.facebook);
-app.set('googleplus', settings.googleplus);
-app.set('bitcointalk', settings.bitcointalk);
-app.set('slack', settings.slack);
-app.set('github', settings.github);
-app.set('website', settings.website);
-app.set('genesis_block', settings.genesis_block);
-app.set('index', settings.index);
-app.set('heavy', settings.heavy);
-app.set('txcount', settings.txcount);
-app.set('nethash', settings.nethash);
-app.set('nethash_units', settings.nethash_units);
-app.set('show_sent_received', settings.show_sent_received);
-app.set('logo', settings.logo);
-app.set('theme', settings.theme);
-app.set('labels', settings.labels);
-
-// catch 404 and forward to error handler
-app.use(function(req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// development error handler
-// will print stacktrace
-if (app.get('env') === 'development') {
-    app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: err
-        });
-    });
-}
-
-// production error handler
-// no stacktraces leaked to user
-app.use(function(err, req, res, next) {
-    res.status(err.status || 500);
-    res.render('error', {
-        message: err.message,
-        error: {}
-    });
-});
+// app.use(function(req, res, next) {
+//     var err = new Error('Not Found');
+//     err.status = 404;
+//     next(err);
+// });
+//
+// // development error handler
+// // will print stacktrace
+// if (app.get('env') === 'development') {
+//     app.use(function(err, req, res, next) {
+//         res.status(err.status || 500);
+//         res.json('error', {
+//             message: err.message,
+//             error: err
+//         });
+//     });
+// }
+//
+// // production error handler
+// // no stacktraces leaked to user
+// app.use(function(err, req, res, next) {
+//     res.status(err.status || 500);
+//     res.json('error', {
+//         message: err.message,
+//         error: {}
+//     });
+// });
 
 module.exports = app;
