d8748c69da8bb85a90a6b4f48cd2c064739b7d65
pinokara <hec.stw@gmail.com>
2019-05-30 01:14:38 +0700

    Constructor of connection object

---
+++ coin/connection.js
@@ -0,0 +1,13 @@
+var settings = require('../lib/settings')
+var mongoose = require('mongoose');
+function Connection(coin) {
+    this.name = coin;
+    var dbString = 'mongodb://' + settings.dbsettings.user;
+    dbString = dbString + ':' + settings.dbsettings.password;
+    dbString = dbString + '@' + settings.dbsettings.address;
+    dbString = dbString + ':' + settings.dbsettings.port;
+    dbString = dbString + '/' + coin;
+    this.connection = mongoose.createConnection(dbString)
+}
+
+module.exports.Connection = Connection;
