579b3159399ca780f549550034ba1250dfa92245
pinokara <hec.stw@gmail.com>
2019-06-13 00:26:48 +0700

    Constructor of peers

--- coin/model/peer.js
+++ coin/model/peer.js
@@ -1,16 +1,20 @@
 var mongoose = require('mongoose')
 
-module.exports = (connection) => connection.model('Peer', new mongoose.Schema({
-  __v: { select: false, type: Number },
-  _id: { required: true, select: false, type: String },
-  country: { type: String },
-  countryCode: { type: String },
-  createdAt: { index: true, required: true, type: Date },
-  ip: { index: true, required: true, type: String },
-  lat: { type: String },
-  lon: { type: String },
-  port: { type: Number },
-  subver: { required: true, type: String },
-  timeZone: { type: String },
-  ver: { required: true, type: Number }
-}, { versionKey: false }), 'peers');
+function Peer(conn) {
+  this.model = conn.model('Peer', new mongoose.Schema({
+    __v: { select: false, type: Number },
+    _id: { required: true, select: false, type: String },
+    country: { type: String },
+    countryCode: { type: String },
+    createdAt: { index: true, required: true, type: Date },
+    ip: { index: true, required: true, type: String },
+    lat: { type: String },
+    lon: { type: String },
+    port: { type: Number },
+    subver: { required: true, type: String },
+    timeZone: { type: String },
+    ver: { required: true, type: Number }
+  }, { versionKey: false }), 'peers')
+}
+
+module.exports.Peer = Peer;
