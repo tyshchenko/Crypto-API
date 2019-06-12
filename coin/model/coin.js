b2bcd45e6e3805df653b2bf6fc06a8a608f51046
pinokara <hec.stw@gmail.com>
2019-06-13 00:25:35 +0700

    Coin model - Stats of coin

--- coin/model/coin.js
+++ coin/model/coin.js
@@ -1,18 +1,22 @@
 const mongoose = require('mongoose');
 
-module.exports = (connection) => connection.model('Coin', new mongoose.Schema({
-    __v: { select: false, type: Number },
-    blocks: { required: true, type: Number },
-    btc: { required: true, type: Number },
-    cap: { required: true, type: Number },
-    createdAt: { index: true, required: true, type: Date },
-    diff: { required: true, type: Number },
-    mnsOff: { required: true, type: Number },
-    mnsOn: { required: true, type: Number },
-    netHash: { required: true, type: Number },
-    peers: { required: true, type: Number },
-    status: { required: true, type: String },
-    supply: { required: true, type: Number },
-    activewallets: { required: false, type: Number },
-    usd: { required: true, type: Number }
-}, { versionKey: false }), 'coins');
+function Coin(conn) {
+    this.model = conn.model('Coin', new mongoose.Schema({
+        __v: { select: false, type: Number },
+        blocks: { required: true, type: Number },
+        btc: { required: true, type: Number },
+        cap: { required: true, type: Number },
+        createdAt: { index: true, required: true, type: Date },
+        diff: { required: true, type: Number },
+        mnsOff: { required: true, type: Number },
+        mnsOn: { required: true, type: Number },
+        netHash: { required: true, type: Number },
+        peers: { required: true, type: Number },
+        status: { required: true, type: String },
+        supply: { required: true, type: Number },
+        activewallets: { required: false, type: Number },
+        usd: { required: true, type: Number }
+    }, { versionKey: false }), 'coins')
+}
+
+module.exports.Coin = Coin;
