43e3e5d1ce6b26f65a53460626bf01817af420bd
pinokara <hec.stw@gmail.com>
2019-06-13 00:29:13 +0700

    Store model by constructor

--- coin/coin.js
+++ coin/coin.js
@@ -1,19 +1,27 @@
 var RPC = require('../lib/rpc');
 var conn = require('./connection');
 var createRouter = require('./router/createRouter');
+var coin = require('./model/coin');
+var block = require('./model/block');
+var masternode = require('./model/masternode');
+var peer = require('./model/peer');
+var rich = require('./model/rich');
+var tx = require('./model/tx');
+var utxo = require('./model/utxo');
+var tank = require('./model/tank')
+// var router = require('./router/createRouter');
 //Contructor create new coin object
 function Coin(object) {
     this.name = object.name;
     this.connection = new conn.Connection(object.name).connection;
     this.rpc = new RPC(object.wallet);
-    this.block = require('./model/block')(this.connection);
-    this.coin = require('./model/coin')(this.connection);
-    this.masternode = require('./model/masternode')(this.connection);
-    this.peer = require('./model/peer')(this.connection);
-    this.rich = require('./model/rich')(this.connection);
-    this.tx = require('./model/tx')(this.connection);
-    this.utxo = require('./model/utxo')(this.connection);
-    this.router = createRouter(this);
+    this.block = new block.Block(this.connection).model;
+    this.coin = new coin.Coin(this.connection).model;
+    this.masternode = new masternode.Masternode(this.connection).model;
+    this.peer = new peer.Peer(this.connection).model;
+    this.rich = new rich.Rich(this.connection).model;
+    this.tx = new tx.TX(this.connection).model;
+    this.utxo = new utxo.UTXO(this.connection).model;
+    // this.router = new router.Router(this).router;
 }
-
 module.exports.Coin = Coin;
