e519deb058460e70552b2845a0e065e9d3536213
pinokara <hec.stw@gmail.com>
2019-06-13 00:25:06 +0700

    Block models - store information of block height

--- coin/model/block.js
+++ coin/model/block.js
@@ -1,17 +1,21 @@
 const mongoose = require('mongoose');
 
-module.exports = (connection) => connection.model('Block', new mongoose.Schema({
-    __v: { select: false, type: Number },
-    bits: { required: true, type: String },
-    confirmations: { required: true, type: Number },
-    createdAt: { index: true, required: true, type: Date },
-    diff: { required: true, type: String },
-    hash: { index: true, required: true, type: String, unique: true },
-    height: { index: true, required: true, type: Number },
-    merkle: { required: true, type: String },
-    nonce: { required: true, type: Number },
-    prev: { required: true, type: String },
-    size: { type: Number },
-    txs: { default: [], required: true, type: [String] },
-    ver: { required: true, type: Number }
-}, { versionKey: false }), 'blocks');
+function Block(conn) {
+    this.model = conn.model('Block', new mongoose.Schema({
+        __v: { select: false, type: Number },
+        bits: { required: true, type: String },
+        confirmations: { required: true, type: Number },
+        createdAt: { index: true, required: true, type: Date },
+        diff: { required: true, type: String },
+        hash: { index: true, required: true, type: String, unique: true },
+        height: { index: true, required: true, type: Number },
+        merkle: { required: true, type: String },
+        nonce: { required: true, type: Number },
+        prev: { required: true, type: String },
+        size: { type: Number },
+        txs: { default: [], required: true, type: [String] },
+        ver: { required: true, type: Number }
+    }, { versionKey: false }), 'blocks')
+}
+
+module.exports.Block = Block;
