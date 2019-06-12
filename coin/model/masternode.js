b243ca7cc95fadb2d689a4577638a27298f5e95b
pinokara <hec.stw@gmail.com>
2019-06-13 00:26:04 +0700

    Masternode models - Store infomation of mns

--- coin/model/masternode.js
+++ coin/model/masternode.js
@@ -1,16 +1,20 @@
 const mongoose = require('mongoose');
 
-module.exports = (connection) => connection.model('Masternode', new mongoose.Schema({
-    __v: { select: false, type: Number },
-    active: { index: true, required: true, type: Number },
-    addr: { index: true, required: true, type: String },
-    createdAt: { required: true, type: Date },
-    lastAt: { required: true, type: Date },
-    lastPaidAt: { index: true, type: Date },
-    network: { type: String },
-    rank: { type: Number },
-    status: { required: true, type: String },
-    txHash: { index: true, required: true, type: String },
-    txOutIdx: { required: true, type: Number },
-    ver: { required: true, type: Number }
-}, { versionKey: false }), 'masternodes');
+function Masternode(conn) {
+    this.model = conn.model('Masternode', new mongoose.Schema({
+        __v: { select: false, type: Number },
+        active: { index: true, required: true, type: Number },
+        addr: { index: true, required: true, type: String },
+        createdAt: { required: true, type: Date },
+        lastAt: { required: true, type: Date },
+        lastPaidAt: { index: true, type: Date },
+        network: { type: String },
+        rank: { type: Number },
+        status: { required: true, type: String },
+        txHash: { index: true, required: true, type: String },
+        txOutIdx: { required: true, type: Number },
+        ver: { required: true, type: Number }
+    }, { versionKey: false }), 'masternodes')
+}
+
+module.exports.Masternode = Masternode;
