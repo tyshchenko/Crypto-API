fb39d55e516ac0553050aa3907325b9da4133866
pinokara <hec.stw@gmail.com>
2019-06-13 00:27:58 +0700

    Constructor of unspent transaction outputs

--- coin/model/utxo.js
+++ coin/model/utxo.js
@@ -1,15 +1,15 @@
 const mongoose = require('mongoose');
 
-const utxoSchema = new mongoose.Schema({
-    __v: { select: false, type: Number },
-    _id: { required: true, select: false, type: String },
-    address: { index: true, required: true, type: String },
-    blockHeight: { index: true, required: true, type: Number },
-    n: { required: true, type: Number },
-    txId: { required: true, type: String },
-    value: { required: true, type: Number }
-}, { versionKey: false });
+function UTXO(conn) {
+    this.model = conn.model('UTXO', new mongoose.Schema({
+        __v: { select: false, type: Number },
+        _id: { required: true, select: false, type: String },
+        address: { index: true, required: true, type: String },
+        blockHeight: { index: true, required: true, type: Number },
+        n: { required: true, type: Number },
+        txId: { required: true, type: String },
+        value: { required: true, type: Number }
+    }, { versionKey: false }), 'utxos');
+}
 
-utxoSchema.index({ address: 1, blockHeight: 1  });
-
-module.exports = (connection) => connection.model('UTXO', utxoSchema, 'utxo');
+module.exports.UTXO = UTXO;
