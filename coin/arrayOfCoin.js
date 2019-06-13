28f5a5119f0d6f4dd160b18e874b917cedbbf62f
pinokara <hec.stw@gmail.com>
2019-06-14 00:50:02 +0700

    Array of coin - back bone of app

--- coin/arrayOfCoin.js
+++ coin/arrayOfCoin.js
@@ -1,4 +1,6 @@
-var array = require('../initial/index');
 var coin = require('./coin');
-
-module.exports = array.map(i => {return new coin.Coin(require(`../initial/${i}`))});
+var array = require('../initial/index');
+var arrayOfCoin = array.map(i => {
+    return new coin.Coin(require(`../initial/${i}`));
+});
+module.exports = arrayOfCoin;
